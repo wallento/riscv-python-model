dfa07cc18d70080b6a36817a8ba62606f87bdb83
Stefan Wallentowitz <stefan@wallentowitz.de>
2018-10-25 16:52:38 +0200

    Fix display of description

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setuptools.setup(
     author_email="stefan@wallentowitz.de",
     description="RISC-V Model",
     long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/wallento/riscv-python-model",
     packages=setuptools.find_packages(),
     entry_points={
