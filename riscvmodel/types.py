09a01b25654d916964d87fc69a692008cdca2ca8
Stefan Wallentowitz <stefan@wallentowitz.de>
2018-10-28 21:02:06 +0100

    Add Register and RegisterFile to types

--- riscvmodel/types.py
+++ riscvmodel/types.py
@@ -122,3 +122,114 @@ class Immediate(object):
         new.set(self.value << shamt)
         return new
 
+
+class Register(object):
+    def __init__(self, bits: int):
+        self.bits = bits
+        self.immutable = False
+        self.value = 0
+        self.format = "{{:0{}x}}".format(int(bits/4))
+        self.mask = (1 << bits) - 1
+
+    def set_immutable(self, s: bool):
+        self.immutable = s
+
+    def randomize(self):
+        if not self.immutable:
+            self.value = randint(0, 1 << self.bits - 1)
+
+    def set(self, value):
+        if isinstance(value, (Register, Immediate)):
+            value = value.value
+        if not self.immutable:
+            self.value = value
+        if (self.value >> (self.bits - 1)) & 1 != 0:
+            self.value |= ~self.mask
+        else:
+            self.value &= self.mask
+
+    def __int__(self):
+        return self.value
+
+    def unsigned(self):
+        return self.value & self.mask
+
+    def __str__(self):
+        return self.format.format(self.value & self.mask)
+
+    def __add__(self, other):
+        new = Register(self.bits)
+        new.set(self.value + int(other))
+        return new
+
+    def __sub__(self, other):
+        new = Register(self.bits)
+        new.set(self.value - int(other))
+        return new
+
+    # todo: doesn't work
+    def __cmp__(self, other):
+        print("cmp")
+        return self.bits != other.bits or self.value != other.value
+
+    def __and__(self, other):
+        new = Register(self.bits)
+        if isinstance(other, int):
+            new.set(self.value & other)
+        elif isinstance(other, (Register, Immediate)):
+            new.set(self.value & other.value)
+        else:
+            raise TypeError("unsupported operand type for Register &: {}".format(other.__class__))
+        return new
+
+    def __or__(self, other):
+        new = Register(self.bits)
+        if isinstance(other, int):
+            new.set(self.value | int)
+        if isinstance(other, (Register, Immediate)):
+            new.set(self.value | other.value)
+        else:
+            raise TypeError("unsupported operand type for Register |: {}".format(other.__class__))
+        return new
+
+
+class RegisterFile(object):
+    def __init__(self, num: int, bits: int, immutable: list = None):
+        self.num = num
+        self.regs = []
+        self.regs_update = []
+        for i in range(num):
+            self.regs.append(Register(bits))
+            self.regs_update.append(Register(bits))
+
+        if isinstance(immutable, list):
+            for r in immutable.items():
+                print(r)
+                self.regs[r[0]].set(r[1])
+                self.regs[r[0]].set_immutable(True)
+                self.regs_update[r[0]].set_immutable(True)
+
+    def randomize(self):
+        for i in range(self.num):
+            self.regs[i].randomize()
+            self.regs_update[i].set(self.regs[i].value)
+
+    def __setitem__(self, key, value):
+        self.regs_update[key].set(value)
+
+    def __getitem__(self, item):
+        return self.regs[item]
+
+    def commit(self):
+        for i in range(self.num):
+            self.regs[i].set(self.regs_update[i].value)
+
+    def changes(self) -> dict:
+        changes = {}
+        for i in range(self.num):
+            if self.regs[i].value != self.regs_update[i].value:
+                changes[i] = (str(self.regs[i]), str(self.regs_update[i]))
+        return changes
+
+    def __str__(self):
+        return "{}".format([str(r) for r in self.regs])
