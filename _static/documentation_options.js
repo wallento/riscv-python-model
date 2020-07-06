9455e125ba7a2728e471034c8118912c635b7671
wallento <wallento@users.noreply.github.com>
2020-07-06 19:40:26 +0000

    deploy: f2d87812e3159ccab23b8dd80aa7577fdbcd2c97

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.3.dev3+g7d2aead',
+    VERSION: '0.4.3.dev4+gf2d8781',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
